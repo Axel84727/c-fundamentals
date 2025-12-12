4ac5be8768f967d9fce96b8fef4a0bbc19f200c4
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-12-11 21:44:11 -0300

    finish ex3.c

--- c-fundamentals/src/01-basics/ex3.c
+++ c-fundamentals/src/01-basics/ex3.c
@@ -12,6 +12,14 @@
 
 int main(void) {
     // Your code here
-    
+    float num1, num2 = 0.0;
+
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+
+    float sum = num1 + num2;
+    printf("The sum of %.2f and %.2f is %.2f\n",num1,num2,sum);
     return 0;
 }
