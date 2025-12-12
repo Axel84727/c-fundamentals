4966bd454f45a28f0730340358e331e1be346047
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-12-11 21:42:22 -0300

    finish ex2.c

--- c-fundamentals/src/01-basics/ex2.c
+++ c-fundamentals/src/01-basics/ex2.c
@@ -12,6 +12,11 @@
 
 int main(void) {
     // Your code here
-    
+    int im_int = 42;
+    float im_float = 3.14f;
+    char im_char = 'A';
+    printf("im an int: %d\n",im_int);
+    printf("im a float: %f\n",im_float);
+    printf("im a char: %c\n",im_char);
     return 0;
 }
