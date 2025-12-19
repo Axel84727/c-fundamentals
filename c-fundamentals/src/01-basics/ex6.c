2a3a5e601f4cb2a3f431659ec44a310c1c87ae70
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-12-19 15:54:46 -0300

    Add circle area and perimeter calculation

--- c-fundamentals/src/01-basics/ex6.c
+++ c-fundamentals/src/01-basics/ex6.c
@@ -12,6 +12,12 @@
 
 int main(void) {
     // Your code here
-    
+    const float pi = 3.14159;
+    printf("Ingrese el radio del circulo: ");
+    float radio;
+    scanf("%f", &radio);
+    float area = pi * radio * radio;
+    printf("El area del circulo es: %.2f\n", area);
+    printf("el perimetro del circulo es: %.2f\n", 2 * pi * radio);
     return 0;
 }
